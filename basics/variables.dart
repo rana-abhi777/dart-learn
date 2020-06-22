edb83c4c5c5937c0726df99fd4bc808fdadef6a4
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-22 19:42:23 +0530

    Learning the basics of Dart - variables

---
+++ basics/variables.dart
@@ -0,0 +1,10 @@
+main(){
+  var fName = 'John';
+  String lName = 'Doe';
+
+  print(fName + ' ' + lName + '.');
+
+  // fName = 10; 
+  // this is an error still as int can't be assigned to string variable
+  print(fName);
+}
